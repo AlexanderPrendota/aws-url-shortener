954313883c05aeda670b10fca3e16242dac04607
Prendota <Prendota@mail.ru>
2018-08-23 10:50:39 +0300

    chore: add validation & use process.env for constants

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
 const AWS = require('aws-sdk');
 const shortid = require('shortid');
-
 const s3 = new AWS.S3();
+
+const BUCKET_NAME = process.env.BUCKET_NAME;
+const REDIRECT_HOST_NAME = process.env.REDIRECT_HOST_NAME;
+const VALIDATION_ERROR_MESSAGE = 'Long url isn\'t found';
+const REQUEST_URL_KEY = 'url';
 
 s3.config.update({
     accessKeyId: process.env.AWS_KEY,
     secretAccessKey: process.env.AWS_SECRET,
-    bucket: "NAME"
+    bucket: BUCKET_NAME
 });
 
 exports.handler = (event, context, callback) => {
-    getShortenLink(event.url, callback)
+    const data = event.data;
+    if (data && data[REQUEST_URL_KEY]) {
+        getShortenLink(event.data.url, callback)
+    } else {
+        fail(callback);
+    }
 };
 
 function getShortenLink(url, callback) {
     let key = shortid.generate();
     let params = {
-        Bucket: "NAME",
+        Bucket: BUCKET_NAME,
         Key: key
     };
     return s3.getObject(params).promise()
-        .then(() => { getShortenLink(url, callback) })
-        .catch(() => { getShort(key, params, url, callback) })
+        .then(() => {
+            getShortenLink(url, callback)
+        })
+        .catch(() => {
+            getShort(key, params, url, callback)
+        })
 }
 
 function getShort(key, params, url, callback) {
@@ -29,6 +42,10 @@ function getShort(key, params, url, callback) {
     params.WebsiteRedirectLocation = url;
 
     return s3.putObject(params).promise()
-        .then(() => callback(null, key))
+        .then(() => callback(null, `${REDIRECT_HOST_NAME}/${key}`))
         .catch(err => console.log(err))
+}
+
+function fail(callback) {
+    callback(VALIDATION_ERROR_MESSAGE, null);
 }
